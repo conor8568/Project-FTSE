5ab1bb748ff780ba573f50936517c185e39e1363
conor8568 <conorlinkston1@gmail.com>
2026-01-14 15:13:57 +0000

    Add package requirements for reproducibility

---
+++ requirements.R
@@ -0,0 +1,11 @@
+# Core data manipulation
+install.packages("tidyverse")
+
+# Financial data
+install.packages("tidyquant")
+
+# Date handling
+install.packages("lubridate")
+
+# Portfolio optimisation
+install.packages("quadprog")
